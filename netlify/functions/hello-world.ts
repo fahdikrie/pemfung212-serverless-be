4e23dcfe9943e27cbfee82a7c26508659e16fd0a
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-11-22 02:38:42 +0700

    feat: add pokedex example

--- netlify/functions/hello-world.ts
+++ netlify/functions/hello-world.ts
@@ -1,4 +1,4 @@
-exports.handler = async function () {
+exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
---
+++ netlify/functions/pokedex.ts
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+exports.handler = async () => {
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+  const response = await fetch(POKE_API, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+  const data = await response.json()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+}
