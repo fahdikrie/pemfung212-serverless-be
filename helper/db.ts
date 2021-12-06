35f689a4614de94fa8ec8cefd277ea52a17c49a8
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-12-06 09:32:04 +0700

    feat(login): work in progress

--- helper/db.ts
+++ helper/db.ts
@@ -5,6 +5,13 @@ const MongoClient = require('mongodb').MongoClient;
 
 let cachedDb: mongoDb.Db | null = null;
 
+export const getDbInfo = () => {
+  const MONGODB_URI = process.env.MONGODB_URI as string;
+  const MONGODB_CLUSTER = process.env.MONGODB_CLUSTER as string;
+
+  return [MONGODB_URI, MONGODB_CLUSTER];
+};
+
 export const connectToDatabase = async (uri: string, db: string) => {
   if (cachedDb) return cachedDb;
 
--- netlify/functions/login.ts
+++ netlify/functions/login.ts
@@ -1,7 +1,18 @@
 import * as mongoDb from 'mongodb';
 import { connectToDatabase, getDbInfo } from '../../helper/db';
 
-exports.handler = async () => {
+interface LoginPayload {
+  username: string;
+  password: string;
+}
+
+exports.handler = async (event: { body: string }) => {
+  const { username, password }: LoginPayload = JSON.parse(event.body);
+
+  if (username && password) {
+    console.log('bundas');
+  }
+
   const [uri, cluster] = getDbInfo();
   const dbClient = (await connectToDatabase(uri, cluster)) as mongoDb.Db;
   const users = await dbClient.collection('user').find({}).toArray();
